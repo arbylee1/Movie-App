d4edf96800cbad8ad1cb1860fa352f5522368eee
Li, Albert <ali74@gatech.edu>
2016-02-08 13:34:28 -0500

    Changed log out button to prevent the user hitting the back button to get back to Main2

--- app/src/main/java/com/gitgood/buzzmovie/Main2Activity.java
+++ app/src/main/java/com/gitgood/buzzmovie/Main2Activity.java
@@ -32,6 +32,7 @@ public class Main2Activity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(i);
             }
         });
