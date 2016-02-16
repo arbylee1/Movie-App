e06f8681b08ab343d2c88626583e36138fccf273
Hotnog, Jeff Janin <jhotnog3@gatech.edu>
2016-02-15 20:40:09 -0500

    Created an infoholder class for users and created an ArrayAdapter for the Listview in the profile .xml file.

--- app/src/main/java/com/gitgood/buzzmovie/ProfileActivity.java
+++ app/src/main/java/com/gitgood/buzzmovie/ProfileActivity.java
@@ -1,20 +1,35 @@
 package com.gitgood.buzzmovie;
 
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //setSupportActionBar(toolbar);
+
+        String[] userProfileData = new String[5]; // String Array that holds user info for Listview
+        // Instantiate info using current user object getters/setters
+        User test = new User("test@gmail.com", "Test", "tester", "CS");
+        userProfileData[0] = test.getEmail();
+        userProfileData[1] = test.getName();
+        userProfileData[2] = test.getUserName();
+        userProfileData[3] = test.getMajor();
+        userProfileData[4] = test.getInterests();
+
+        // Links the user data to the view through an ArrayAdapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getListView().getContext(), R.layout.content_profile, userProfileData);
+        getListView().setAdapter(adapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
