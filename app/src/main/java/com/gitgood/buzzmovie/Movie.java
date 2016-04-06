f759f0cabf2922531ad7dad0e3d47bb1abf5353d
Albert Li <ali74@gatech.edu>
2016-04-06 13:52:13 -0400

    Revert "Merge branch 'master' of github.gatech.edu:Git-Good-CS2340/BuzzMovie"
    This reverts commit 3f79d71a731115015bc8ee9a6c000fd18652593a, reversing
    changes made to 0f865bdce5e135299617d8531631d90f9ef6fdf9.

--- app/src/main/java/com/gitgood/buzzmovie/Movie.java
+++ app/src/main/java/com/gitgood/buzzmovie/Movie.java
@@ -6,28 +6,18 @@ import java.io.Serializable;
  * Created by Chudy on 2/21/16.
  */
 public class Movie implements Serializable {
-    String title;
-    String rottenTomatoID;
-    String year;
-    String rating;
-    String synopsis;
-    float numRatings = 0;
-    float averageRating = 0.0f;
+    private String title;
+    private String rottenTomatoID;
+    private String year;
+    private String rating;
+    private String synopsis;
+    private float numRatings = 0;
+    private float averageRating = 0.0f;
 
-    /* Trivial constructor for Movie object
-    * @param movie String value of movie
-    * @param Id String value of Id
-    * @param year String value of year it was released
-    * @param rating String rt overall rating for movie
-    * @param sysnopsis String value of rt movie summary
-    * @param numRating how many rating has been submitted for this movie
-    * @param averageRating float value of rating out of 5.0
-    *
-     */
-    public Movie(String movie, String Id, String year, String rating, String synopsis, float numRatings
-          ,float averageRating) {
+    public Movie(String movie, String id, String year, String rating, String synopsis, float numRatings
+                 ,float averageRating) {
         this.title = movie;
-        this.rottenTomatoID = Id;
+        this.rottenTomatoID = id;
         this.year = year;
         this.rating = rating;
         this.synopsis = synopsis;
@@ -35,36 +25,19 @@ public class Movie implements Serializable {
         this.averageRating = averageRating;
     }
 
-    /*
-    * getter method for AverageRating
-    * @return float of averageRating Variable
-     */
     public final float getAverageRating() {
         return averageRating;
     }
-    /*
-    * getter method for AverageRating
-    * @return float of numRating Variable
-     */
+
     public final float getNumRatings() {return numRatings;}
 
-    /*
-    * method containg logic for adding a rating to the movie
-    * @param stars float rating out of 5
-    * @param major String value of major how set the rating
-    * @param User String username of the user that set the rating
-     */
-    public final void addRating(float stars, String major, String User) {
+    public final void addRating(float stars, String major, String user) {
         averageRating *= numRatings;
         averageRating += stars;
         numRatings++;
         averageRating /= numRatings;
     }
 
-    /*
-    * method containing logic to remove a rating
-    * @param stars float rating out of 5 to be removed
-     */
     public final void removeRating(float stars) {
         averageRating *= numRatings;
         averageRating -= stars;
@@ -72,49 +45,31 @@ public class Movie implements Serializable {
         averageRating /= numRatings;
     }
 
-    /*
-    * getter method for movie movie title
-    * @return String value of title
-     */
     public final String getMovie() {
         return title;
     }
 
-    /*
-    * getter method for movie's movie title
-    * @return String value of rt id
-     */
     public final String getRottenTomatoID() {
         return rottenTomatoID;
     }
 
-    /*
-    * getter method for movie's release year
-    * @return String value of release year
-    */
     public final String getYear() {
         return year;
     }
 
-    /*
-     * getter method for movie's summary
-     * @return String value of movie summary
-     */
     public final String getSynopsis() {return synopsis;}
 
-    /*
-     * getter method for movie's overall rating
-     * @return String value of movie rating
-     */
     public final String getRating() {
         return rating;
     }
 
-    /*
-     * method to formaulate movie object as string
-     * @return String value of summation of movie object
-     */
     public final String toString() {
         return "Name: " + title + " (" + year + "), Rating: " + rating;
     }
+
+//    public Map<String, Rating> GetRating(SharedPreferences movieData){
+//        this.movieData = movieData;
+//        return new Map<String, Rating>;
+//    }
+
 }
